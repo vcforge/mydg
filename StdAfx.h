b6aa18b5536af57507d80214bdc646eb67e60dbf
vcforge <vcforge@yahoo.com>
2015-06-12 10:32:42 +0800

    引入Store类

--- StdAfx.h
+++ StdAfx.h
@@ -13,6 +13,11 @@
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
 
 // TODO: reference additional headers your program requires here
 #include <windows.h>
